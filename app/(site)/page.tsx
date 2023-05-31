5acc877a65988236995276530eccd0fe84d07f39
vinaydeep26 <vinaydeep26@gmail.com>
2023-05-31 16:50:16 -0400

    Changes to be committed: 	modified:   app/(site)/page.tsx 	modified:   components/ListItem.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -40,7 +40,18 @@ export default function Home() {
               href="liked" 
             />
           </div>
-        </div></Header>
+        </div>
+        </Header>
+        <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+LIST OF SONGS
+        </div>
+    </div>
     </div>
   )
 }
