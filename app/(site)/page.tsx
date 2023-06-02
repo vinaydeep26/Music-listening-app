40f4af9430d6baac9a8b35bb50585f05577cc07b
vinaydeep26 <vinaydeep26@gmail.com>
2023-06-02 16:36:45 -0400

    new file:   actions/getSongs.ts 	new file:   actions/getSongsByUserId.ts 	new file:   app/(site)/components/PageContent.tsx 	modified:   app/(site)/page.tsx 	modified:   app/layout.tsx 	modified:   components/Library.tsx 	new file:   components/MediaItem.tsx 	new file:   components/PlayButton.tsx 	modified:   components/Sidebar.tsx 	new file:   components/SongItem.tsx 	new file:   hooks/useLoadImage.ts 	new file:   middleware.ts 	modified:   next.config.js

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,11 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
+export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div
       className="
@@ -48,9 +52,7 @@ export default function Home() {
             Newest songs
           </h1>
         </div>
-        <div>
-LIST OF SONGS
-        </div>
+        <PageContent songs={songs} />
     </div>
     </div>
   )
--- components/Library.tsx
+++ components/Library.tsx
@@ -4,7 +4,14 @@ import { AiOutlinePlus} from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
-const Library = () => {
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+interface LibraryProps {
+  songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -35,7 +42,14 @@ const Library = () => {
         />
           </div>
           <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of SOngsS
+          {songs.map((item) => (
+            <MediaItem 
+            onClick={() => {}} 
+            key={item.id} 
+            data={item}
+          />
+            
+            ))}
       </div>
           </div>
   )
